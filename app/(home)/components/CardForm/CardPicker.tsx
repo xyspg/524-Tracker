89577429a5a91641cb45b84b9e0a5380e647cde8
Kang Jiaming <jiaming_kang@outlook.com>
2024-11-30 14:08:05 +0800

    fix: layout on mobile

--- app/(home)/components/CardForm/CardPicker.tsx
+++ app/(home)/components/CardForm/CardPicker.tsx
@@ -54,7 +54,7 @@ export function CardPicker({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[300px] p-0">
+      <PopoverContent className="w-[300px] h-[200px] md:h-[300px] p-0">
         <Command>
           <CommandInput placeholder="Search card..." />
           <CommandList>
